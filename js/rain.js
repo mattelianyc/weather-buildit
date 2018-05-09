280a174d2cd33a3a3287468179d526d32e0cc2cb
EC2 Default User <ec2-user@ip-172-31-13-206.us-east-2.compute.internal>
2018-05-09 14:19:39 +0000

    google map greyscale and thensome - fog animations, cloud algo, much needed function refactoring

--- js/rain.js
+++ js/rain.js
@@ -10,12 +10,12 @@ function randRange( minNum, maxNum) {
 function createRain() {
 
 	for( i=1;i<nbDrop;i++) {
-	var dropLeft = randRange(0,1600);
-	var dropTop = randRange(-1000,1400);
+		var dropLeft = randRange(0,1600);
+		var dropTop = randRange(-1000,1400);
 
-	$('.rain').append('<div class="drop" id="drop'+i+'"></div>');
-	$('#drop'+i).css('left',dropLeft);
-	$('#drop'+i).css('top',dropTop);
+		$('.rain').append('<div class="drop" id="drop'+i+'"></div>');
+		$('#drop'+i).css('left',dropLeft);
+		$('#drop'+i).css('top',dropTop);
 	}
 
 }
